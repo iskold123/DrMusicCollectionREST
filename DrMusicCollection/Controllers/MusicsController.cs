ae8af69df6c3b0ad17e43b2be0d41c77d51a5a25
Mads Olsen <mads36g6@edu.easj.dk>
2020-10-19 11:49:08 +0200

    GetAll oprettet

--- DrMusicCollection/Controllers/MusicsController.cs
+++ DrMusicCollection/Controllers/MusicsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ModelLib.Model;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,36 +13,43 @@ namespace DrMusicCollection.Controllers
     [ApiController]
     public class MusicsController : ControllerBase
     {
+
+        private static List<Music> _data = new List<Music>()
+        {
+            new Music("sådan der", "Tessa", 180, 2019),
+            new Music("Ung Kniv", "Minds of 99", 175, 2018)
+        };
+
         // GET: api/<MusicsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Music> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _data;
         }
 
-        // GET api/<MusicsController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //// GET api/<MusicsController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
-        // POST api/<MusicsController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        //// POST api/<MusicsController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
 
-        // PUT api/<MusicsController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        //// PUT api/<MusicsController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
-        // DELETE api/<MusicsController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/<MusicsController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
