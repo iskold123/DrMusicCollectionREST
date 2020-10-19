736cad50dc5cef4b68375e0760681b80546155f6
muha1992 <58588384+muha1992@users.noreply.github.com>
2020-10-19 11:23:49 +0200

    Instans felter og modelklasse oprettet

---
+++ ModelLib/Model/Music.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLib.Model
+{
+    public class Music
+    {
+        private string _title;
+        private string _artist;
+        private int _duration;
+        private int _yearOfPublication;
+    }
+}
