8d04bb5359d4f98277f86533bd6da23564a1ffbb
Mads Olsen <mads36g6@edu.easj.dk>
2020-10-19 11:33:25 +0200

    Music done, MusicController oprettet

---
+++ DrMusicCollection/Controllers/MusicsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DrMusicCollection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicsController : ControllerBase
+    {
+        // GET: api/<MusicsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MusicsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MusicsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MusicsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MusicsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- ModelLib/Model/Music.cs
+++ ModelLib/Model/Music.cs
@@ -10,5 +10,47 @@ namespace ModelLib.Model
         private string _artist;
         private int _duration;
         private int _yearOfPublication;
+
+        public Music()
+        {
+
+        }
+
+        public Music(string title, string artist, int duration, int yearOfPublication)
+        {
+            _title = title;
+            _artist = artist;
+            _duration = duration;
+            _yearOfPublication = yearOfPublication;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public string Artist
+        {
+            get => _artist;
+            set => _artist = value;
+        }
+
+        public int Duration
+        {
+            get => _duration;
+            set => _duration = value;
+        }
+
+        public int YearOfPublication
+        {
+            get => _yearOfPublication;
+            set => _yearOfPublication = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Title)}: {Title}, {nameof(Artist)}: {Artist}, {nameof(Duration)}: {Duration}, {nameof(YearOfPublication)}: {YearOfPublication}";
+        }
     }
 }
