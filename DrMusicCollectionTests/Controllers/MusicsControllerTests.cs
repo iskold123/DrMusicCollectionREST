9a303bb189c7a554020bb12b20ec7c36bd7a0463
muha1992 <58588384+muha1992@users.noreply.github.com>
2020-10-20 11:47:45 +0200

    Test af getall done

--- DrMusicCollectionTests/Controllers/MusicsControllerTests.cs
+++ DrMusicCollectionTests/Controllers/MusicsControllerTests.cs
@@ -3,20 +3,27 @@ using DrMusicCollection.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ModelLib.Model;
 
 namespace DrMusicCollection.Controllers.Tests
 {
     [TestClass()]
     public class MusicsControllerTests
     {
+        private MusicsController cntr = new MusicsController();
 
         [TestInitialize]
+
+        public void BeforeEachMethod()
+        {
+            cntr = new MusicsController();
+        }
 
 
         [TestMethod()]
         public void GetTest()
         {
-            Assert.Fail();
+            Assert.AreEqual(2,new List<Music>(cntr.Get()).Count);
         }
     }
 }
